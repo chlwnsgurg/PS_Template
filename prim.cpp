f2841d97170d8370dd96b7ad6dea6c9aafff8dec
chlwnsgurg <chlwnsgurg@gmail.com>
2023-11-22 23:52:15 +0900

    dijkstra added

--- prim.cpp
+++ prim.cpp
@@ -26,7 +26,7 @@ const ll INF=INT64_MAX;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-     ll v,e;cin>>v>>e;
+    ll v,e;cin>>v>>e;
     vector<vector<pll>> adj(v);
     for(ll i=0;i<e;i++){
         ll a,b,c;
